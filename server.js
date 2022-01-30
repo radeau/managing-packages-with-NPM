9e29b81c4722761e0fb6dd1c36f04fc1da355228
radeau <kevinradp@gmail.com>
2022-01-30 22:08:24 +0400

    Add middleware function to add style.css

--- server.js
+++ server.js
@@ -68,6 +68,9 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 })
 
+//Task 3 - Serve Static Assets
+app.use('/public', express.static(__dirname + "/public"));
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
